dac18979b06596de9ec4a77ec13d9d97f54ccf43
Peter Jones <pjones@devalot.com>
2025-05-23 12:51:32 +0200

    bug: Make checkbox hook buffer local

--- org-grader.el
+++ org-grader.el
@@ -121,7 +121,7 @@ STRUCT is taken from `org-list-struct'."
         (progn
           (org-num-mode -1)
           (add-to-list 'org-capture-templates template)
-          (add-hook 'org-checkbox-statistics-hook #'org-grader-points-property-update))
+          (add-hook 'org-checkbox-statistics-hook #'org-grader-points-property-update nil t))
       (let ((templates (remove template org-capture-templates)))
         (setq org-capture-templates templates)
         (remove-hook 'org-checkbox-statistics-hook #'org-grader-points-property-update t)))))
